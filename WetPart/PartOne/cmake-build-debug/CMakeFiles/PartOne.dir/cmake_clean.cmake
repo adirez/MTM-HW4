2a8c0c9156f869f96f04f9d6dbef9d499b79ffe0
adirez <adireznik@campus.technion.ac.il>
2017-06-28 19:21:51 +0300

    first commit

---
+++ WetPart/PartOne/cmake-build-debug/CMakeFiles/PartOne.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PartOne.dir/main.cpp.obj"
+  "PartOne.pdb"
+  "PartOne.exe"
+  "PartOne.exe.manifest"
+  "libPartOne.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PartOne.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
